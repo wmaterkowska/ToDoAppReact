34d211a81a6fa3c93b48bab727316b47d467dbe6
Weronika Materkowska <w.m.materkowska@gmail.com>
2023-09-08 01:31:51 +0200

    add todoCard

--- src/components/TodoCard/TodoCard.tsx
+++ src/components/TodoCard/TodoCard.tsx
@@ -1,12 +1,53 @@
+import "./TodoCard.css"
 import type { Todo } from "types";
+import { useState } from "react";
+import dayjs from "dayjs";
+import relativeTime from 'dayjs/plugin/relativeTime';
 
-export default function ToDoCard({ todo }: { todo: Todo }) {
+
+dayjs.extend(relativeTime);
+interface TodoCardProps {
+  todo: Todo;
+}
+
+const TodoCard: React.FC<TodoCardProps> = ({ todo }) => {
+
+
+  const [isChecked, setIsChecked] = useState(todo.done);
+  const [toEdit, setToEdit] = useState(false);
+
+  const todoDate = dayjs(todo.lastChange).fromNow();
+
+
+  function handleCheckboxChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const updatedTodo = { ...todo };
+    updatedTodo.done = event.target.checked;
+    setIsChecked(updatedTodo.done)
+    console.log("test");
+  }
+
+  function handleClickOnCard() {
+    setToEdit(true);
+  }
+
+  function handleBackArrow(event: React.MouseEvent) {
+    event.stopPropagation();
+    // event.preventDefault();
+    setToEdit(false);
+  }
 
   return (
-    <div>
-      <p>{todo.title}</p>
-      <p>{todo.content}</p>
-      <p>{todo.lastChange}</p>
+    <div className={`todo-container ${toEdit ? "todo-container-edit" : "todo-container-noedit"}`} onClick={handleClickOnCard}>
+      {toEdit && <button className="back" onClick={handleBackArrow}>--</button>}
+      <div className={`${isChecked ? "todo-done" : "todo"}`}>
+        <p className="todo-title">{todo.title}</p>
+        <p className="todo-content">{todo.content}</p>
+        <p className="todo-date">{todoDate}</p>
+      </div>
+      <input type="checkbox" defaultChecked={isChecked} onChange={handleCheckboxChange} />
+      {toEdit && <button className="delete">delete</button>}
     </div>
   )
 }
+
+export default TodoCard;
