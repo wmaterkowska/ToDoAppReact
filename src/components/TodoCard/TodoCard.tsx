b3f7c28d8e99dd17c1e175c8f8eb6e6291c7b6b4
Weronika Materkowska <w.m.materkowska@gmail.com>
2023-09-08 20:05:55 +0200

    add css to todocard

--- src/components/TodoCard/TodoCard.tsx
+++ src/components/TodoCard/TodoCard.tsx
@@ -35,18 +35,28 @@ const TodoCard: React.FC<TodoCardProps> = ({ todo }) => {
   }
 
   return (
-    <div className={`todo-container ${toEdit ? "todo-container-edit" : "todo-container-noedit"}`} onClick={handleClickOnCard}>
-      {toEdit && <button className="back" onClick={handleBackArrow}>--</button>}
-      <div className={`${isChecked ? "todo-done" : "todo"}`}>
-        <p className="todo-title">{todo.title}</p>
-        <p className="todo-content">{todo.content}</p>
-        <p className="todo-date">{todoDate}</p>
+    <>
+      <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght@300" rel="stylesheet" />
+
+      <div className={`todo-container ${toEdit ? "todo-container-edit" : "todo-container-noedit"}`} onClick={handleClickOnCard}>
+
+        <label className="checkbox-container" onClick={(event) => event.stopPropagation()}>
+          <input type="checkbox" defaultChecked={isChecked} onChange={handleCheckboxChange} className={`checkbox ${toEdit ? "checkbox-edit" : "checkbox-noedit"}`} />
+        </label>
+
+        <nav className="navigation">
+          {toEdit && <button className="back" onClick={handleBackArrow}><span class="material-symbols-outlined">arrow_back_ios</span></button>}
+          {toEdit && <button className="delete"><span class="material-symbols-outlined">delete</span></button>}
+        </nav>
+
+        <div className={`${isChecked ? "todo-done" : "todo"}`}>
+          <p className="todo-title">{todo.title}</p>
+          <p className="todo-content">{todo.content}</p>
+          <p className="todo-date">{todoDate}</p>
+        </div>
+
       </div>
-      <label onClick={(event) => event.stopPropagation()}>
-        <input type="checkbox" defaultChecked={isChecked} onChange={handleCheckboxChange} />
-      </label>
-      {toEdit && <button className="delete">delete</button>}
-    </div>
+    </>
   )
 }
 
